29c5e5d3b00a23acc219f24c5ef5640bb572ffc0
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-09-23 23:50:12 +0200

    All content was created + bootstrap & icons imported

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import CONTENT from "./content";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container-sm">
       <h1 style={{textAlign: "center"}}>{CONTENT.title}</h1>
       {CONTENT.steps.map((step, i) => 
         <div key={"step" + i} >
-          <h3>{i + 1}. {step.title}</h3>
-          <ul>{step.content.map((item, n) => <li key={"li" + n}>{item}</li>)}</ul>
+          <h3 className="text-center">{i + 1}. {step.title}</h3>
+          <div className="row">
+            <div className="col-lg-3"></div>
+            <div className="col-lg-6"><ul>{step.content.map((item, n) => <li key={"li" + n}>{item}</li>)}</ul></div>
+            <div className="col-lg-3"></div>
+          </div>
         </div>
       )}
     </div>
--- src/content.js
+++ src/content.js
@@ -25,16 +25,44 @@ const CONTENT = {
         {
             title: "Create the App you want to build",
             content: [
-                "Create the App you want to build in your cleared create-react-app template"
+                "Create the App you want to build in your cleared create-react-app template & when it will be ready to publish..."
             ]
         },
         {
             title: "Publish your App with GitHub Pages deployment",
             content: [
-                ""
+                `Open your package.json and add a homepage field for your project: "homepage": "https://myusername.github.io/my-app",`,
+                `Run "npm install --save gh-pages" in your Visual Code Studio Terminal`,
+                `Add the following scripts in your package.json "scripts": "predeploy": "npm run build" & "deploy": "gh-pages -d build"`,
+                "Publish your repository (or commit changes in existing repository)",
+                `Run: "npm run deploy" in your Visual Code Studio Terminal`,
+            ]
+        },
+        {
+            title: "Check gh-pages deployment in your GitHub App repository",
+            content: [
+                "Check if there is a gh-pages branch in your App repository",
+                "Click github-pages of your repository (you'll see it near the rocket icon on the right) and then...",
+                `Click "View Deployment" & check if everything renders correctly`,
+                "Celebrate!"
+            ]
+        },
+        {
+            title: "PS. Adding Bootstrap & Bootstrap Icons into project",
+            content: [
+                `Run: "npm install --save bootstrap" in your Visual Code Studio Terminal`,
+                `Import Bootstrap CSS in the beginning of your src/index.js file: "import 'bootstrap/dist/css/bootstrap.css';"`,
+                `Run: "npm i bootstrap-icons" in your Visual Code Studio Terminal`,
+                `Import Bootstrap Icons CSS in the beginning of your src/index.js file: 'import "bootstrap-icons/font/bootstrap-icons.css";'`
             ]
         }
     ]
 }
+
+/*
+https://create-react-app.dev/docs/deployment/
+https://create-react-app.dev/docs/adding-bootstrap/
+https://www.npmjs.com/package/bootstrap-icons
+*/
 
 export default CONTENT;
